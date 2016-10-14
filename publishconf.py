fabc588cd0a695d18675e2bbda727a3446f47f77
jswh <jswh@jswh.me>
2016-10-14 17:21:58 +0800

    write article

--- publishconf.py
+++ publishconf.py
@@ -10,8 +10,6 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = ''
-RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
