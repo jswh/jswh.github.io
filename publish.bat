6e9dd5d28de99c874a45136d00b51b95eddfd6d6
jswh <jswh@jswh.me>
2016-12-10 16:44:18 +0800

    write article

--- publish.bat
+++ publish.bat
@@ -4,6 +4,6 @@ git add .
 git commit -a -m "write article"
 git push origin pelican
 cp %CDIR%\CNAME %CDIR%\output\CNAME
-ghp-import -m "Generate Pelican site" -b master %CDIR%\output
+python %CDIR%\Scripts\ghp-import -m "Generate Pelican site" -b master %CDIR%\output
 git push origin master
 pause
