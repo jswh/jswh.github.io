2fb98720348927061b3dd9115f04632e637a2622
jswh <jswh@jswh.me>
2016-12-10 16:56:14 +0800

    write article

--- publish.bat
+++ publish.bat
@@ -4,6 +4,8 @@ git add .
 git commit -a -m "write article"
 git push origin pelican
 cp %CDIR%\CNAME %CDIR%\output\CNAME
+::git reve-parse
+::for /f %%i in ('git rev-list --max-count=1 origin/master') do set GIT_HEADER=%%i
 python %CDIR%\Scripts\ghp-import -m "Generate Pelican site" -b master %CDIR%\output
 git push origin master
 pause
